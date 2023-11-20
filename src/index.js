049459c1480190529ea53ee098ccbe3bdc1d49ad
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-11-20 18:49:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/quiz-master">
+      <BrowserRouter basename="/node_front_bc_55_Stepaniuk-Den">
         <App />
       </BrowserRouter>
     </PersistGate>
